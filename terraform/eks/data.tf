dddc861affe4a7c5f00a01e86db8996f42850098
Damian Tykalowski <damian@d47zm3.me>
2023-05-17 09:14:18 +0200

    Split Into Terraform For State And Terraform For EKS

--- terraform/eks/data.tf
+++ terraform/eks/data.tf
@@ -28,10 +28,16 @@ data "aws_iam_policy_document" "external_secrets_operator" {
 
 data "aws_eks_cluster" "eks" {
   name = module.eks.cluster_name
+
+  depends_on = [
+    module.eks
+  ]
 }
 
 data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_name
-}
 
-data "github_ip_ranges" "this" {}
+  depends_on = [
+    module.eks
+  ]
+}
--- terraform/eks/terraform.tf
+++ terraform/eks/terraform.tf
@@ -14,7 +14,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "tsh-eks-demo-terraform-state"
-    key            = "state/terraform.tfstate"
+    key            = "eks/terraform.tfstate"
     region         = "eu-west-1"
     encrypt        = true
     kms_key_id     = "alias/terraform_bucket_key"
@@ -35,10 +35,10 @@ provider "aws" {
   }
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
+#provider "kubernetes" {
+#  host                   = data.aws_eks_cluster.eks.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#  token                  = data.aws_eks_cluster_auth.eks.token
+#}
 
 provider "github" {}
