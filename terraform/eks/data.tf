f17590010490bcaaf2c56a2b1ff3fc90605ce624
Damian Tykalowski <damian@d47zm3.me>
2023-06-15 14:57:03 +0200

    remove depends from data

--- terraform/eks/data.tf
+++ terraform/eks/data.tf
@@ -28,16 +28,8 @@ data "aws_iam_policy_document" "external_secrets_operator" {
 
 data "aws_eks_cluster" "eks" {
   name = module.eks.cluster_name
-
-  depends_on = [
-    module.eks
-  ]
 }
 
 data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_name
-
-  depends_on = [
-    module.eks
-  ]
 }
